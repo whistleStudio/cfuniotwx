557d5b664c02e1993536ac2dfbc0cb42de9d10bc
whistleStudio <whistleStudio@126.com>
2022-05-30 17:34:55 +0800

    05301734

--- iotwx/main.js
+++ iotwx/main.js
@@ -4,18 +4,18 @@ import App from './App'
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
+
+import uView from "uview-ui"
+Vue.use(uView)
+uni.$u.config.unit = 'rpx'
+
 const app = new Vue({
     ...App
 })
 app.$mount()
 // #endif
 
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
+
+
+
+
--- iotwx/pages/index/index.vue
+++ iotwx/pages/index/index.vue
@@ -1,8 +1,17 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="content flex-center">
+		<view class="logbox">
+			<image src="../../static/img/login/cflogo.png" mode="widthFix"></image>
+			<text>创趣物联小程序</text>
+			<view class="uni-form-item uni-column mgt-50">
+				<view class="title">用户邮箱</view>
+				<input class="uni-input mgt-20" focus placeholder="name@example.com" placeholder-class="text-plh"/>
+			</view>	
+			<view class="uni-form-item uni-column mgt-20">
+				<view class="title">密码</view>
+				<input class="uni-input mgt-20" password type="text" placeholder-class="text-plh"/>
+			</view>
+			<button  class="mgt-50" hover-class="btn-hover">登录</button>
 		</view>
 	</view>
 </template>
@@ -23,30 +32,48 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	// @import "@/static/css/v.scss";
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		height: 100vh;
+		.logbox {
+			width: 600rpx;
+			height: 750rpx;
+			border: 1px solid $mainColorAA;
+			border-radius: 5px;
+			position: relative;
+			box-sizing: border-box;
+			padding: 0 50rpx;
+			color: $fontC;
+			>image {
+				width: 170rpx;
+				position: absolute;
+				top: 40rpx;
+				left: 50%;
+				transform: translateX(-50%);
+			}
+			>text {
+				width: 100%;
+				text-align: center;
+				display: block;
+				margin-top: 180rpx;
+				font: 45rpx $fontF;
+			}
+			input {
+				border: 1px solid rgb(180,180,180);
+				border-radius: 5px;
+				text-indent: 10rpx;
+				font: 40rpx/50rpx;
+			}
+			button {
+				font: 30rpx/80rpx $fontF;
+				background-color: $mainColorA;
+				color: white;
+			}
+		}
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	
+	.btn-hover {
+		background-color: $mainColor !important;
 	}
 </style>
